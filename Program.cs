e517a244f360c934525c9ae4790734aa8481abfa
marioaraujougb <mario.araujo@ugb.edu.br>
2023-10-27 14:42:55 -0300

    Teste

--- Program.cs
+++ Program.cs
@@ -26,7 +26,6 @@ namespace TesteUGB
             builder.Services.AddMvc();
             builder.Services.AddControllersWithViews();
 
-
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
