8a20df6e65ddd1d72e84495d6ecab4b1dde34473
MARIO ARAUJO <mario.araujo89@hotmail.com>
2023-10-29 16:28:26 -0300

    Compras Criada com sucesso, funcional.

--- Models/ComprasModel.cs
+++ Models/ComprasModel.cs
@@ -14,7 +14,7 @@ namespace TesteUGB.Models
         public DateTime DataPrevisaoEntregaProduto { get; set; }
         public TipoDoProdutoEnum TipoDoProduto { get; set; }
         public int ValorUnitarioDoProduto { get; set; }
-        public int ValorTotal { get; set; } // Soma do valor unitario + quantidade
+        public int ValorTotal { get; set; } // Soma do valor unitario x quantidade
         public long NumeroNotaFiscalProduto { get; set; }
         public long CodigoEAN { get; set; } // Código de Barras do Produto
     }
